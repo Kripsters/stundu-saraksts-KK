ebd83f2d32cc2979b033e43f8327d985ba704fa7
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-09 13:51:50 +0300

    ULDI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 import "./App.css";
 import Diena from "./Diena.js";
-const d = new Date();
-function weekDay() {
-  return d.toLocaleDateString("lv", { weekday: "long" });
-}
-
-function monthLongName() {
-  return d.toLocaleDateString("lv", { month: "long" });
-}
 
 const pirmdienasStundas = [
   "Sociālās zinības",
@@ -23,20 +15,69 @@ const otrdienasStundas = [
   "2Krievu valoda",
 ];
 
-const visasStundas = {
-  Pirmdiena: ["Sociālās zinības", "Dabaszinības", "Vēsture", "Krievu valoda"],
-};
+const visasStundas = [
+  {
+    diena: "Pirmdiena",
+    stundas: ["Sociālās zinības", "Dabaszinības", "Vēsture", "Krievu valoda"],
+  },
+  {
+    diena: "Otrdiena",
+    stundas: [
+      "2Sociālās zinības",
+      "2Dabaszinības",
+      "2Vēsture",
+      "2Krievu valoda",
+    ],
+  },
+  {
+    diena: "Trešdiena",
+    stundas: [
+      "3Sociālās zinības",
+      "3Dabaszinības",
+      "3Vēsture",
+      "3Krievu valoda",
+    ],
+  },
+  {
+    diena: "Ceturtdiena",
+    stundas: [
+      "4Sociālās zinības",
+      "4Dabaszinības",
+      "4Vēsture",
+      "4Krievu valoda",
+    ],
+  },
+  {
+    diena: "Piektdiena",
+    stundas: [
+      "5Sociālās zinības",
+      "5Dabaszinības",
+      "5Vēsture",
+      "5Krievu valoda",
+    ],
+  },
+];
+
+const dienasJSX = visasStundas.map((diena, indekss) => {
+  return <Diena key={indekss} diena={diena.diena} stundas={diena.stundas} />;
+});
 
 export default function App() {
   return (
     <>
       <div>
-        <Diena stundas={pirmdienasStundas} />
-        <br />
-        <br />
-        <br />
-        <Diena stundas={otrdienasStundas} />
+        <ol>{dienasJSX}</ol>
       </div>
     </>
   );
 }
+//        <Diena stundas={pirmdienasStundas} />
+//       <Diena stundas={otrdienasStundas} />
+//const d = new Date();
+//function weekDay() {
+//  return d.toLocaleDateString("lv", { weekday: "long" });
+//}
+//
+//function monthLongName() {
+//  return d.toLocaleDateString("lv", { month: "long" });
+//}
